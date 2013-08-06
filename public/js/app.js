8cfb178864f8ead20aa0609878b20379c2904d57
Luis Hurtado <luis@hlmbusiness.com>
2013-08-06 14:55:53 -0500

    Add final step and enhance UI.

--- public/js/app.js
+++ public/js/app.js
@@ -3,16 +3,23 @@ $(function(){
 
   if (!localStorage.answers) localStorage.answers = JSON.stringify([]);
 
-  $('div.step .start-experience').on('click', function(e){
+  $('.fb-share').on('click', function(e){
     e.preventDefault();
 
-    // get current answer and next step
-    var step = $(this).parents('div.step');
-    var nextStep = step.next();
-
-    // animate transition
-    step.fadeOut(200, function(){
-      nextStep.fadeIn(200);
+    FB.ui({
+      method: 'feed',
+      display: 'touch',
+      link: 'http://www.combia.com.co',
+      picture: 'http://res.cloudinary.com/hlm-business-inc/image/upload/c_scale,h_200/v1375757246/combia_noiba6.jpg',
+      name: 'Combia - Inspiración',
+      caption: 'Descubre el alma del café',
+      description: 'Acabo de vivir la mejor experiencia de mi vida en el corazón de la cultura cafetera',
+    }, function(response){
+      if( response != null && response.error_code == null ) {
+        console.log('Acabas de inspirar!');
+      } else {
+        console.log('Inspira a tu familia y amigos a través de la vivencia del café');
+      }
     });
   });
 
@@ -23,37 +30,21 @@ $(function(){
     var answer = $(this);
     var step = answer.parents('div.step');
 
-    if( step.hasClass('last-step') ) {
-      FB.ui({
-        method: 'feed',
-        display: 'touch',
-        link: 'http://www.combia.com.co',
-        picture: 'http://res.cloudinary.com/hlm-business-inc/image/upload/v1375743725/LOGO-HACIENDA-HOTEL_aagk5l.png',
-        name: 'Combia - Inspiración',
-        caption: 'Descubre el alma del café',
-        description: 'Acabo de vivir la mejor experiencia de mi vida en el corazón de la cultura cafetera',
-      }, function(response){
-        if( response != null && response.error_code == null ) {
-          alert('Acabas de inspirar!');
-        } else {
-          alert('Inspira a tu familia y amigos a través de la vivencia del café');
-        }
-      });
+    var nextStep = step.next();
 
-    } else {
-      var nextStep = step.next();
+    // save the answer
+    var answerData = answer.attr('data-answer');
 
-      // save the answer
+    if( answerData != '' ) {
       var answers = JSON.parse(localStorage["answers"]);
-      answers.push(answer.attr('data-answer'));
+      answers.push(answerData);
       localStorage["answers"] = JSON.stringify(answers);
-
-      // animate transition
-      step.fadeOut(200, function(){
-        nextStep.fadeIn(200);
-      });
     }
 
+    // animate transition
+    step.fadeOut(200, function(){
+      nextStep.fadeIn(200);
+    });
   });
 
 });
